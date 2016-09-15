264e01a8e611e71c0115c7f753c95c0d1e9c54e7
Dan Stutzman <dan@danstutzman.com>
2016-09-15 13:09:21 -0600

    Use *.danstutzman.com domains instead of *.internal through /etc/host file.  Also use /root/prometheus_configs/*.yml files for configs instead of editing /root/prometheus.yml directly.

--- 3_setup_prometheus.sh
+++ 3_setup_prometheus.sh
@@ -8,32 +8,29 @@ if [ ! -e prometheus-1.1.2.linux-amd64 ]; then
   tar xvfz prometheus-1.1.2.linux-amd64.tar.gz
 fi
 
-grep -v ' (basicruby|monitoring).internal ' /etc/hosts > /etc/hosts.new
-echo '192.81.208.111 basicruby.internal' >> /etc/hosts.new
-echo '127.0.0.1 monitoring.internal' >> /etc/hosts.new
-mv /etc/hosts.new /etc/hosts
+mkdir -p /root/prometheus_configs
+
+echo "- targets: [ 'vocabincontext.danstutzman.com:9100' ]" \
+  >/root/prometheus_configs/vocabincontext.yml
 
 tee prometheus.yml <<EOF2
 global:
-  scrape_interval:     15s # By default, scrape targets every 15 seconds.
-
-  # Attach these labels to any time series or alerts when communicating with
-  # external systems (federation, remote storage, Alertmanager).
-  external_labels:
-    monitor: 'codelab-monitor'
-
-# A scrape configuration containing exactly one endpoint to scrape:
-# Here it's Prometheus itself.
+  scrape_interval: 15s
 scrape_configs:
-  # The job name is added as a label job=<job_name> to any timeseries scraped from this config.
-  - job_name: 'prometheus'
-
-    # Override the global default and scrape targets from this job every 5 seconds.
-    scrape_interval: 5s
-
-    static_configs:
-      - targets: ['monitoring.internal:9100', 'basicruby.internal:9100']
-
+- job_name: file_sd_configs
+  file_sd_configs:
+  - files:
+    - /root/prometheus_configs/*.yml
+    refresh_interval: 1m
+- job_name: node_exporter
+  static_configs:
+    - targets:
+      - monitoring.danstutzman.com:9100
+      - basicruby.danstutzman.com:9100
+- job_name: prometheus-piwik-exporter
+  static_configs:
+    - targets:
+      - localhost:9101
 rule_files:
 - '/root/alert.rules'
 EOF2
