09618716fcd44eccdb7ed3660571202edc6de4ad
Dan Stutzman <dan@danstutzman.com>
2016-09-15 21:03:12 -0600

    Setup HTTPS for grafana on monitoring.danstutzman.com:3000.

--- 5_setup_grafana.sh
+++ 5_setup_grafana.sh
@@ -1,8 +1,17 @@
 #!/bin/bash -ex
+DOMAINS_AND_TLS=~/dev/domains_and_tls
+
+$DOMAINS_AND_TLS/renew_certificate.sh monitoring.danstutzman.com
+
+INSTANCE_IP=`tugboat droplets | grep 'monitoring ' | egrep -oh "[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+" || true`
+scp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -P 2222 \
+  $DOMAINS_AND_TLS/tls/certs/monitoring.danstutzman.com/cert.pem \
+  $DOMAINS_AND_TLS/tls/certs/monitoring.danstutzman.com/privkey.pem \
+  root@$INSTANCE_IP:/etc/grafana
 
 tugboat ssh -n monitoring <<EOF
-
 set -ex
+
 wget https://grafanarel.s3.amazonaws.com/builds/grafana_3.1.1-1470047149_amd64.deb
 sudo apt-get install -y adduser libfontconfig
 sudo dpkg -i grafana_3.1.1-1470047149_amd64.deb
@@ -11,7 +20,15 @@ if [ ! -e /etc/grafana/grafana.ini.bak ]; then
   sudo cp /etc/grafana/grafana.ini /etc/grafana/grafana.ini.bak
 fi
 
-cat /etc/grafana/grafana.ini.bak | sed 's/;\?allow_sign_up = \\(.*\\)/allow_sign_up = false/' | sudo tee /etc/grafana/grafana.ini
+chown root:grafana /etc/grafana/*.pem
+chmod g+r /etc/grafana/*.pem
+
+cat /etc/grafana/grafana.ini.bak \
+  | sed 's/^;\?allow_sign_up =\\(.*\\)/allow_sign_up = false/' \
+  | sed 's/^;\?protocol =\\(.*\\)/protocol = https/' \
+  | sed 's/^;\?cert_file =\\(.*\\)/cert_file = \/etc\/grafana\/cert.pem/' \
+  | sed 's/^;\?cert_key =\\(.*\\)/cert_key = \/etc\/grafana\/privkey.pem/' \
+  | sudo tee /etc/grafana/grafana.ini
 
 sudo service grafana-server restart
 sudo update-rc.d grafana-server defaults 95 10
