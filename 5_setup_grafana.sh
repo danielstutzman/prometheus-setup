4989cf0ce46cba0b22ef64d2a7acd0e3a93ba3b6
Dan Stutzman <dan@danstutzman.com>
2016-09-15 20:32:51 -0600

    Expose grafana over port 3000 instead of requiring port forwarding.

--- 5_setup_grafana.sh
+++ 5_setup_grafana.sh
@@ -7,7 +7,17 @@ wget https://grafanarel.s3.amazonaws.com/builds/grafana_3.1.1-1470047149_amd64.d
 sudo apt-get install -y adduser libfontconfig
 sudo dpkg -i grafana_3.1.1-1470047149_amd64.deb
 
+if [ ! -e /etc/grafana/grafana.ini.bak ]; then
+  sudo cp /etc/grafana/grafana.ini /etc/grafana/grafana.ini.bak
+fi
+
+cat /etc/grafana/grafana.ini.bak | sed 's/;\?allow_sign_up = \\(.*\\)/allow_sign_up = false/' | sudo tee /etc/grafana/grafana.ini
+
 sudo service grafana-server restart
 sudo update-rc.d grafana-server defaults 95 10
 
+sudo ufw allow 3000
+
 EOF
+
+echo "Please login with admin/admin and set the admin's password"
