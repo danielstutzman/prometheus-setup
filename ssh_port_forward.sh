b37463797a8ae1956f0e69a4f93d17ecb2a48f16
Dan Stutzman <dan@danstutzman.com>
2016-09-24 21:15:55 -0600

    Port forward 9093 (alertmanager) too.

--- ssh_port_forward.sh
+++ ssh_port_forward.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -ex
-tugboat ssh monitoring -o "-L 9090:localhost:9090 -L 9100:localhost:9100"
+tugboat ssh monitoring -o "-L 9090:localhost:9090 -L 9100:localhost:9100 -L 9093:localhost:9093"
