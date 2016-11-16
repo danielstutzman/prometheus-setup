4de540cceb19bd0ca13300180b634a915966b11b
Dan Stutzman <dan@danstutzman.com>
2016-11-15 21:34:31 -0700

    Combined amd64 and 386 version of setup_node_exporter.  Also run services under node_exporter user instead of root.

--- 4_setup_node_exporter.sh
+++ 4_setup_node_exporter.sh
@@ -1,22 +1,37 @@
 #!/bin/bash -ex
 
-fwknop -s -n monitoring.danstutzman.com
-tugboat ssh -n monitoring <<EOF
+#for INSTANCE in basicruby monitoring vocabincontext; do
+for INSTANCE in monitoring; do
+  fwknop -s -n $INSTANCE.danstutzman.com
+  ssh root@$INSTANCE.danstutzman.com <<"EOF"
+  set -ex
 
-if [ ! -e node_exporter-0.12.0.linux-amd64 ]; then
-  curl -L https://github.com/prometheus/node_exporter/releases/download/0.12.0/node_exporter-0.12.0.linux-amd64.tar.gz > node_exporter-0.12.0.linux-amd64.tar.gz
-  tar xvzf node_exporter-0.12.0.linux-amd64.tar.gz
-fi
+  id -u node_exporter &>/dev/null || sudo useradd node_exporter
+  sudo mkdir -p /home/node_exporter
+  sudo chown node_exporter:node_exporter /home/node_exporter
+  cd /home/node_exporter
 
-tee /etc/init/node_exporter.conf <<EOF2
-start on startup
-script
-  /root/node_exporter-0.12.0.linux-amd64/node_exporter
-end script
+  if [ `uname -p` == i686 ];     then ARCH=386
+  elif [ `uname -p` == x86_64 ]; then ARCH=amd64; fi
+
+  if [ ! -e node_exporter-0.12.0.linux-$ARCH ]; then
+    curl -L https://github.com/prometheus/node_exporter/releases/download/0.12.0/node_exporter-0.12.0.linux-$ARCH.tar.gz > node_exporter-0.12.0.linux-$ARCH.tar.gz
+    chown node_exporter:node_exporter node_exporter-0.12.0.linux-$ARCH.tar.gz
+    sudo -u node_exporter tar xvzf node_exporter-0.12.0.linux-$ARCH.tar.gz
+  fi
+
+  tee /etc/init/node_exporter.conf <<EOF2
+  start on filesystem
+  setuid node_exporter
+  setgid node_exporter
+  script
+    /home/node_exporter/node_exporter-0.12.0.linux-$ARCH/node_exporter
+  end script
 EOF2
 
-sudo ufw allow from \$(dig +short monitoring.danstutzman.com) to any port 9100
+  sudo ufw allow from $(dig +short monitoring.danstutzman.com) to any port 9100
 
-sudo service node_exporter restart
+  sudo service node_exporter restart
 
 EOF
+done
