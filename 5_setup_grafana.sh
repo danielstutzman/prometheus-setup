5dfade0a87b125e58153371612b8048e255c46f7
Dan Stutzman <dan@danstutzman.com>
2016-12-26 21:53:12 -0500

    Access grafana at https://grafana.monitoring.danstutzman.com

--- 5_setup_grafana.sh
+++ 5_setup_grafana.sh
@@ -1,13 +1,7 @@
 #!/bin/bash -ex
 DOMAINS_AND_TLS=~/dev/domains_and_tls
 
-$DOMAINS_AND_TLS/renew_certificate.sh monitoring.danstutzman.com
-
-fwknop -s -n monitoring.danstutzman.com
-scp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null \
-  $DOMAINS_AND_TLS/tls/certs/monitoring.danstutzman.com/cert.pem \
-  $DOMAINS_AND_TLS/tls/certs/monitoring.danstutzman.com/privkey.pem \
-  root@monitoring.danstutzman.com:/etc/grafana
+$DOMAINS_AND_TLS/create_route53_A_record_to_cloudfront_alias.sh danstutzman.com grafana.monitoring.danstutzman.com
 
 tugboat ssh -n monitoring <<EOF
 set -ex
@@ -20,21 +14,14 @@ if [ ! -e /etc/grafana/grafana.ini.bak ]; then
   sudo cp /etc/grafana/grafana.ini /etc/grafana/grafana.ini.bak
 fi
 
-chown root:grafana /etc/grafana/*.pem
-chmod g+r /etc/grafana/*.pem
-
 cat /etc/grafana/grafana.ini.bak \
   | sed 's/^;\?allow_sign_up =\\(.*\\)/allow_sign_up = false/' \
-  | sed 's/^;\?protocol =\\(.*\\)/protocol = https/' \
-  | sed 's/^;\?cert_file =\\(.*\\)/cert_file = \/etc\/grafana\/cert.pem/' \
-  | sed 's/^;\?cert_key =\\(.*\\)/cert_key = \/etc\/grafana\/privkey.pem/' \
+  | sed 's/^;\?http_port =\\(.*\\)/http_port = 3000/' \
   | sudo tee /etc/grafana/grafana.ini
 
 sudo service grafana-server restart
 sudo update-rc.d grafana-server defaults 95 10
 
-sudo ufw allow 3000
-
 EOF
 
-echo "Please login to Grafana at https://monitoring.danstutzman.com:3000 with admin/admin and set the admin's password"
+echo "Please login to Grafana at https://grafana.monitoring.danstutzman.com with admin/admin and set the admin's password"
